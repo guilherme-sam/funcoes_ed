91875adbdfb0c6f721fdd0e5c44a756412af65ff
guilherme-sam <guilherme_de_lima@hotmail.com>
2020-03-04 21:54:21 -0300

    exercicio 03

---
+++ ex_functions03.c
@@ -0,0 +1,25 @@
+void maior_numero (int a , int b){
+    if(a > b )
+        printf("\n ",a);
+    else
+    printf("\n %d", b );
+
+    
+}
+void maior_numero_v2(int a , int b){
+    printf("\n %d", a > b > a : b );
+}
+
+
+void maior_numero_v3 (int a , int b){
+    switch (a > b){
+        case 0 ; 
+            printf("\n %d",b);
+            break;
+        
+        default:
+            printf("\n %d", a ); 
+            break;
+    }
+}
+    
