4b6801a3a623cef0b43e52ffd92b7a9466e3cde0
guilherme-sam <guilherme_de_lima@hotmail.com>
2020-03-04 21:54:03 -0300

    exercicio 02

---
+++ ex_functions02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//procedimento
+void calcula_e_exibe_medio_ponderada (float n1 , float n2, float n3 , int o1, int p2 , int p3){
+    float aux = (n1 * p1 + n2 * p2  + n3 * p3 ) / (p1+p2+p3);
+    printf("\nResultado %.2f", aux);
+
+}
+
+
